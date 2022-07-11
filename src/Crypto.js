c306bb1f08261494d0869e7e480fc5b8a0302aee
kpelc91 <kamilapelc01@gmail.com>
2022-07-11 07:46:11 +0200

    state i tablica newCryptoList

--- src/Crypto.js
+++ src/Crypto.js
@@ -6,9 +6,50 @@ import CryptoList from './CryptoList';
 
 class Crypto extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            cryptoList: []
+        };
+    }
+
+    componentDidMount() {
+        this.getCryptoData();
+    }
+
+    getCryptoData = () => {
+
+        axios.get('https://blockchain.info/ticker')
+            .then(res => {
+                const tickers = res.data;
+
+                this.setState((state) => {
+                    let newCryptoList = [];
+
+                    for (const [ticker, cryptoRate] of Object.entries(tickers)) {
+                        let newCryptoObj = {
+                            currency: ticker, symbol: cryptoRate.symbol,
+                            buy: cryptoRate.buy,
+                            sell: cryptoRate.sell,
+                            lastRate: cryptoRate.last,
+                        }
+
+                        newCryptoList.push(newCryptoObj);
+                    }
+
+                    return({
+                        cryptoList: newCryptoList
+                    })
+                });
+
+                console.log(res.data);
+            });
+    }
+
     render() {
         return(
-            <div clasName="Crypto">
+            <div className="Crypto">
                 <CryptoList />
             </div>
         );
